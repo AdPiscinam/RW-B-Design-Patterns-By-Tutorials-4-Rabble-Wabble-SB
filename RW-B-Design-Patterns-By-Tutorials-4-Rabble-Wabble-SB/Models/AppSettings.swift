596529f2e4ee1bd91f30cfe86e1cf1ee5fc964d2
AdPiscinam <lab@walimaloui.com>
2022-03-15 23:39:44 +0100

    Add AppSettingsViewController File

--- RW-B-Design-Patterns-By-Tutorials-4-Rabble-Wabble-SB/Models/AppSettings.swift
+++ RW-B-Design-Patterns-By-Tutorials-4-Rabble-Wabble-SB/Models/AppSettings.swift
@@ -5,8 +5,30 @@ public class AppSettings {
   // MARK: - Static Properties
   public static let shared = AppSettings()
   
+  // MARK: - Instance Properties
+  public var questionStrategyType: QuestionStrategyType {
+    get {
+      let rawValue = userDefaults.integer(forKey: Keys.questionStrategy)
+      return QuestionStrategyType(rawValue: rawValue)!
+    } set {
+      userDefaults.set(newValue.rawValue, forKey: Keys.questionStrategy)
+    }
+  }
+  
+  private let userDefaults = UserDefaults.standard
+  
   // MARK: - Object Lifecycle
   private init() { }
+  
+  // MARK: - Instance Methods
+  public func questionStrategy(for questionGroup: QuestionGroup) -> QuestionStrategy {
+    return questionStrategyType.questionStrategy(for: questionGroup)
+  }
+}
+
+// MARK: - Keys
+private struct Keys {
+  static let questionStrategy = "questionStrategy"
 }
 
 // MARK: - QuestionStrategyType
@@ -27,13 +49,11 @@ public enum QuestionStrategyType: Int, CaseIterable {
   
   public func questionStrategy(
     for questionGroup: QuestionGroup) -> QuestionStrategy {
-    switch self {
-    case .random:
-      return RandomQuestionStrategy(
-        questionGroup: questionGroup)
-    case .sequential:
-      return SequentialQuestionStrategy(
-        questionGroup: questionGroup)
+      switch self {
+      case .random:
+        return RandomQuestionStrategy(questionGroup: questionGroup)
+      case .sequential:
+        return SequentialQuestionStrategy(questionGroup: questionGroup)
+      }
     }
-  }
 }
